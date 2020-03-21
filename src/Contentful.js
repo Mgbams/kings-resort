f6b901a90ebd679104ee201aa2be29ecd16e6b1f
kingsley <cmgbams@gmail.com>
2020-03-21 13:46:18 +0100

    setting up environment variables

--- src/Contentful.js
+++ src/Contentful.js
@@ -1,6 +1,6 @@
 import { createClient } from 'contentful';
 
 export default createClient({
-    space: "nz39o0x9j86k",
-    accessToken: "1tVqD2D4TZwurAD3Z3UOzHEI4otoJMdZezQ8TxHhOns"
-})
+    space: process.env.REACT_APP_API_SPACE,
+    accessToken: process.env.REACT_APP_ACCESS_TOKEN
+});
